777de907d0e55d9f1414fe5600de16ac7aaa18ea
Rydur01 <rydur87@gmail.com>
2023-09-18 23:51:52 -0400

    Added stuff from Module 4 Videos
    added  all the scripts and game objects from Module 4

---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.UI;
+
+public class LobbyManager : NetworkBehaviour
+{
+    public Button startbutton;
+    public TMPro.TMP_Text statusLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startbutton.gameObject.SetActive(false);
+        statusLabel.text = "Start something, like the server or the host or the client";
+
+        startbutton.onClick.AddListener(OnStartButtonClicked);
+        NetworkManager.OnClientStarted += OnClientStarted;
+        NetworkManager.OnServerStarted += OnServerStarted;
+    }
+
+    private void OnServerStarted()
+    {
+        startbutton.gameObject.SetActive(true);
+        statusLabel.text = "Press Start";
+    }
+
+    private void OnClientStarted()
+    {
+        if (!IsHost) 
+        {
+            statusLabel.text = "Waiting for game to start";
+        }
+        
+    }
+
+    private void OnStartButtonClicked()
+    {
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        NetworkManager.SceneManager.LoadScene(
+            "Arena1Game",
+            UnityEngine.SceneManagement.LoadSceneMode.Single);
+    }
+}
