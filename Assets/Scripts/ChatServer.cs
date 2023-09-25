11f913c46ded818564fbef2f9cc643b76b5928ac
Rydur01 <rydur87@gmail.com>
2023-09-25 19:52:59 -0400

    Demo Features
    Each player gets greeting message from the server upon connecting (5 points)
    Host can send messages to all connected players (5 points)
    Client can send message to all connected players (5 points)
    Message displays the sender's client ID for receivers and "you" for sender (5 points)
    Can send direct messages using player id (clientId) (5 points)

---
+++ Assets/Scripts/ChatServer.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class ChatServer : NetworkBehaviour
+{
+    public ChatUi chatUi;
+    const ulong SYSTEM_ID = ulong.MaxValue;
+    private ulong[] dmClientIds = new ulong[2];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        chatUi.printEnteredText = false;
+        chatUi.MessageEntered += OnChatUiMessageEntered;
+
+        if (IsServer)
+        {
+            NetworkManager.OnClientConnectedCallback += ServerOnClientConnected;
+            if (IsHost)
+            {
+                DisplayMessageLocally(SYSTEM_ID, $"You are the host And client {NetworkManager.LocalClientId}");
+            }
+            else
+            {
+                DisplayMessageLocally(SYSTEM_ID, "You are the server");
+            }
+        }
+        else
+        {
+            DisplayMessageLocally(SYSTEM_ID, $"You are the client {NetworkManager.LocalClientId}");
+        }
+    }
+
+    private void ServerOnClientConnected(ulong clientId)
+    {
+        ServerSendDirectMessage(
+            $"I ({NetworkManager.LocalClientId}) see you ({clientId}) have connected to the server, well done",
+            NetworkManager.LocalClientId,
+            clientId);
+    }
+
+    private void DisplayMessageLocally(ulong from, string message)
+    {
+        string fromStr = $"Player {from}";
+        Color textColor = chatUi.defaultTextColor;
+
+        if(from == NetworkManager.LocalClientId)
+        {
+            fromStr = "you";
+            textColor = Color.magenta;
+        }
+        else if(from == SYSTEM_ID)
+        {
+            fromStr = "SYS";
+            textColor = Color.green;
+        }
+
+        chatUi.addEntry(fromStr, message, textColor);
+    }
+
+    private void OnChatUiMessageEntered(string message)
+    {
+        SendChatMessageServerRpc(message);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void SendChatMessageServerRpc(string message, ServerRpcParams serverRpcParams = default)
+    {
+        // "@123 hello world this is a longer message"
+        if (message.StartsWith("@"))
+        {
+            string[] parts = message.Split(" ");
+            string clientIdStr = parts[0].Replace("@", "");
+            ulong toClientId = ulong.Parse(clientIdStr);
+
+            ServerSendDirectMessage(message, serverRpcParams.Receive.SenderClientId, toClientId);
+        }
+        else
+        {
+            RecieveChatMessageClientRpc(message, serverRpcParams.Receive.SenderClientId);
+        }
+        
+    }
+
+    [ClientRpc]
+    public void RecieveChatMessageClientRpc(string message, ulong from, ClientRpcParams clientRpcParams = default)
+    {
+        DisplayMessageLocally(from, message);
+    }
+
+    private void ServerSendDirectMessage(string message, ulong from, ulong to)
+    {
+        dmClientIds[0] = from;
+        dmClientIds[1] = to;
+
+        ClientRpcParams rpcParams = default;
+        rpcParams.Send.TargetClientIds = dmClientIds;
+
+        RecieveChatMessageClientRpc($"<whisper> {message}", from, rpcParams);
+    }
+}
